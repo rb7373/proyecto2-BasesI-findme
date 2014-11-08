a9f65d50ab61dac487a65df3b05147745f499254
Yasiell <yasi1205@hotmail.com>
2014-11-08 11:59:23 -0600

    Script
    Script cambia el tipo de datos

---
+++ Scripts de Registros/alter(typeData).sql
@@ -0,0 +1,4 @@
+ALTER TABLE donacionesxcasacuna MODIFY vacunas INTEGER;
+ALTER TABLE donacionesxcasacuna MODIFY Alimento INTEGER;
+ALTER TABLE donacionesxcasacuna MODIFY transporte INTEGER;
+ALTER TABLE donacionesxcasacuna MODIFY dinero INTEGER;
