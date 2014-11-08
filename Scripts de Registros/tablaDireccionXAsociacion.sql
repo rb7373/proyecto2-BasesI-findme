dc19f26a631857e8574074f0cf5875b5f883a7e0
Yasiell <yasi1205@hotmail.com>
2014-11-08 12:40:57 -0600

    Tabla DireccionXAsociacion

---
+++ Scripts de Registros/tablaDireccionXAsociacion.sql
@@ -0,0 +1,18 @@
+-- -----------------------------------------------------
+-- Table `petsrescue`.`DireccionXAsociacion`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `petsrescue`.`DireccionXAsociacion` ;
+
+CREATE TABLE IF NOT EXISTS `petsrescue`.`DireccionXAsociacion` (
+  `idDireccionXAsociacion` INT NOT NULL AUTO_INCREMENT,
+  `idAsociaciones` INT NOT NULL,
+  `idDireccion` INT NOT NULL,
+  `FechaDeCreacion` DATE NULL,
+  `FechaDeModificacion` DATE NULL,
+  `CreadoPor` INT NULL,
+  `ModificadoPor` INT NULL,
+  PRIMARY KEY (`idDireccionXAsociacion`))
+ENGINE = InnoDB;
+
+ALTER TABLE direccionxasociacion add foreign key (idAsociaciones) references asociaciones(idAsociaciones);
+alter table direccionxasociacion add foreign key (idDireccion) references direccion(idDireccion);
