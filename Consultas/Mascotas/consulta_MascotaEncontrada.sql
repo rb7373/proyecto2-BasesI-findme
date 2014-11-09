bee0bb5ce86318aa932197f6f59171df98bca584
Treicysg <treicysanchez.27@gmail.com>
2014-11-09 16:11:22 -0600

    Consultar x dirección
    Se le agrega a consultar mascotas

--- Consultas/Mascotas/consulta_MascotaEncontrada.sql
+++ Consultas/Mascotas/consulta_MascotaEncontrada.sql
@@ -1,16 +1,25 @@
 use petsrescue;
-
+#Se utilizara idRescatador para mostrar información de la persona
 
 DROP PROCEDURE IF EXISTS consultar_mascotaEncontrada;
 
 DELIMITER //
 
 CREATE PROCEDURE consultar_mascotaEncontrada( ptipoMascota varchar(100),pnombreMascota varchar(100),pnumero_chip int,
-prazaMascota varchar(45),pcolorMascota varchar(50), pFechaEncuentro date)
+prazaMascota varchar(45),pcolorMascota varchar(50), pFechaEncuentro date,
+pnombreProvincia varchar(45),pNombreCanton varchar(100))
 
 begin
 
-	select * from mascotasencontradas
+	select idMascota,fecha_encuentro,idRescatador,idFotoMascotaEncontrada,Observaciones from mascotasencontradas
+    inner join direccionxreporteencuentro
+    on direccionxreporteencuentro.mascotaEncontrada_id = mascotasencontradas.idMascotasEncontradas
+    inner join direccion
+    on direccion.idDireccion_pk = direccionxreporteencuentro.direccionEncuentro_id
+	inner join provincia
+	on provincia.idProvincia = direccion.provincia_id
+    inner join canton
+    on canton.idCanton = direccion.canton_id
     inner join mascota
     on mascotasencontradas.idMascotas = mascota.idMascota
 	inner join tipomascota
@@ -21,8 +30,9 @@ begin
 	on colormascota.idColorMascota = mascota.idColorMascota
     where mascotasencontradas.fecha_encuentro <= ifnull( pFechaEncuentro,mascotasencontradas.fecha_encuentro) and mascota.nombre_mascota like ifnull( concat('%',pnombreMascota ,'%'), mascota.nombre_mascota) 
 	and mascota.chipNumber_mascota = ifnull(pnumero_chip, mascota.chipNumber_mascota ) and tipomascota.tipo = ifnull(ptipoMascota, tipomascota.tipo )  and razamascota.raza like ifnull(concat('%', prazaMascota ,'%'), razamascota.raza) 
-	and colormascota.Color = ifnull(pcolorMascota ,colormascota.Color)
-    order by mascota.nombre_mascota;
+	and colormascota.Color = ifnull(pcolorMascota ,colormascota.Color) and provincia.nombre_provincia = pnombreProvincia
+    and canton.nombre_canton = pNombreCanton
+    order by mascotasencontradas.fecha_encuentro;
 
 
 end  //
--- Consultas/Mascotas/consulta_MascotaPerdida.sql
+++ Consultas/Mascotas/consulta_MascotaPerdida.sql
@@ -1,17 +1,28 @@
 use petsrescue;
 
+#Se ordena por fecha(desde + reciente a más antiguo)
+#Se puede consultar por dirección(direccionxreporteencuentro)
+
 
 DROP PROCEDURE IF EXISTS consultar_mascotaPerdida;
 
 DELIMITER //
 
 CREATE PROCEDURE consultar_mascotaPerdida ( ptipoMascota varchar(100),pnombreMascota varchar(100),pnumero_chip int,
-prazaMascota varchar(45),pcolorMascota varchar(50), pFechaExtravio date)
+prazaMascota varchar(45),pcolorMascota varchar(50), pFechaExtravio date,pnombreProvincia varchar(45),pNombreCanton varchar(100))
 
 begin
 
-	select * from mascotasperdidas
-    inner join mascota
+	select idDueno,fecha_perdida,recompensa,idFotoMascotaPerdida from mascotasperdidas
+    inner join direccionxreporteperdida
+    on direccionxreporteperdida.mascotaPerdida_id = mascotasperdidas.idMascotasPerdidas
+    inner join direccion
+    on direccion.idDireccion_pk = direccionxreporteperdida.direccionPerdida_id
+	inner join provincia
+	on provincia.idProvincia = direccion.provincia_id
+    inner join canton
+    on canton.idCanton = direccion.canton_id
+	inner join mascota
     on mascotasperdidas.idMascota = mascota.idMascota
 	inner join tipomascota
 	on tipomascota.idTipoMascota = mascota.idTipoMascota
@@ -21,8 +32,9 @@ begin
 	on colormascota.idColorMascota = mascota.idColorMascota
     where mascotasperdidas.fecha_perdida <= ifnull(pFechaExtravio, mascotasperdidas.fecha_perdida) and mascota.nombre_mascota like ifnull( concat('%', pnombreMascota ,'%'), mascota.nombre_mascota) 
 	and mascota.chipNumber_mascota = ifnull(pnumero_chip, mascota.chipNumber_mascota ) and tipomascota.tipo = ifnull(ptipoMascota, tipomascota.tipo )  and razamascota.raza like ifnull(concat('%', prazaMascota ,'%'), razamascota.raza) 
-	and colormascota.Color = ifnull(pcolorMascota ,colormascota.Color)
-    order by mascota.nombre_mascota;
+	and colormascota.Color = ifnull(pcolorMascota ,colormascota.Color) and provincia.nombre_provincia = pnombreProvincia
+    and canton.nombre_canton = pNombreCanton
+    order by mascotasperdidas.fecha_perdida;
 
 
 end  //
