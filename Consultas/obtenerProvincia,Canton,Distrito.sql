3fc04382bd20d3baa6649e7ed4caeccca1030b69
Yasiell <yasi1205@hotmail.com>
2014-11-09 15:06:46 -0600

    Cambio consulta de provincia, distrito y canton

--- Consultas/obtenerProvincia,Canton,Distrito.sql
+++ Consultas/obtenerProvincia,Canton,Distrito.sql
@@ -8,6 +8,7 @@ DROP PROCEDURE IF EXISTS proc_obtenerProvincia;
 
 DELIMITER //
 
+
 CREATE PROCEDURE proc_obtenerProvincia() 
 
 begin
@@ -26,11 +27,14 @@ DROP PROCEDURE IF EXISTS proc_obtenerCanton;
 
 DELIMITER //
 
-CREATE PROCEDURE proc_obtenerCanton( idprovincias int) 
+CREATE PROCEDURE proc_obtenerCanton( provincia varchar(100)) 
 
 begin
+	declare temp_provinciaID int;
+	select obtener_provincia_id(provincia) into temp_provinciaID;
+
 	select nombre_canton from canton
-	where idProvincia = idprovincias;
+	where idProvincia = temp_provinciaID;
 
 end  //
 
@@ -44,11 +48,15 @@ DROP PROCEDURE IF EXISTS proc_obtenerDistrito;
 
 DELIMITER //
 
-CREATE PROCEDURE proc_obtenerDistrito( idcantones int) 
+CREATE PROCEDURE proc_obtenerDistrito(canton varchar(100)) 
 
 begin
+	declare temp_cantonID int;
+
+	select obtener_canton_id(canton) into temp_cantonID;
+
 	select nombre_distrito from distrito
-	where idCanton = idcantones;
+	where idCanton = temp_cantonID;
 
 end  //
 
