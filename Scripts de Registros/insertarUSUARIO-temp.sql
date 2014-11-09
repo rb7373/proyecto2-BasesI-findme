ab5c706534368a0edde0ed7482880a00904c9290
Ruben Abarca Navarro <abarca.ruben73@gmail.com>
2014-11-08 18:48:45 -0600

    Cambios en el diagrama

---
+++ Scripts de Registros/insertarUSUARIO-temp.sql
@@ -0,0 +1,8 @@
+USE presentacion;
+#Insertar tipousuario
+
+call proc_insertar_curso('Matematica');
+call proc_insertar_curso('Frances');
+call proc_insertar_curso('Bases');
+
+call proc_insertar_usuario('Maria',3);
