044a4cc62509d69ac961eb7c4e49fe671766949c
Yasiell <yasi1205@hotmail.com>
2014-11-10 01:08:57 -0600

    Página de Ayuda

---
+++ Interfaz/ayuda.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<head>
+	<meta charset="utf-8" />
+	<title>PetRescue</title>
+     <link rel="shortcut icon" href= "img/favicon.ico" type="image/x-icon" />
+    
+    <link rel="stylesheet" href="ayuda.css" media="screen">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+
+		<style>
+			body{
+				background: #000;
+				color: #DDD;
+				font-family: 'Helvetica', 'Lucida Grande', 'Arial',"sans-serif";
+			}
+		</style>
+        
+        <style>
+#wrapper {
+	margin-top: 50px;
+	margin-right: auto;
+	margin-left: auto;
+	margin-bottom: 0;
+	width: 940px;
+}
+ 
+#footer {
+
+        color: #fff;
+        height: 100px;
+        text-align: center;
+        width: 940px;
+}
+        </style>        
+        
+</head>
+<body>
+   
+    
+<div align="center">
+  
+</div> 
+<div align="right">
+  <!-- end .menu -->
+      
+</div>
+
+
+<div id="wrapper">
+	<div id="content">
+	<div id="form-main"  align="center">
+      <h2 id="registroExitoso">Para mayor información</h2>
+     <a href="http://petrescue.wikia.com/wiki/PetRescue_Wiki">Presione acá<a>
+  	</div>
+	</div>
+ 
+ 
+	<div id="footer">
+	<p align="right">
+  					    </p>
+	</div>
+</div>
+
+</body>
+</html>
