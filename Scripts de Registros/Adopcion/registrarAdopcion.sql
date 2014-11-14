1d067eb3cca4bfd24668d56f96df79c788d4e4f6
Treicysg <treicysanchez.27@gmail.com>
2014-11-14 10:24:10 -0600

    Registro Adopción , Trigger adopción
    Se cambia el registro adopción, se agrega su trigger

--- Scripts de Registros/Adopcion/registrarAdopcion.sql
+++ Scripts de Registros/Adopcion/registrarAdopcion.sql
@@ -7,12 +7,12 @@ DROP PROCEDURE IF EXISTS proc_registrar_Adopcion;
 DELIMITER //
 
 CREATE PROCEDURE proc_registrar_Adopcion(
-   padoptante_id int  ,pmascota_id int)
+   padoptante_id int  ,pmascota_id int,pCalificacion int)
 
 begin
 
-INSERT INTO adopciones(adoptante_id,mascota_id)
-VALUES(padoptante_id,pmascota_id);
+INSERT INTO adopciones(adoptante_id,mascota_id, calificacion)
+VALUES(padoptante_id,pmascota_id, pCalificacion);
 
 end  //
 
